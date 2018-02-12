fac83e3c5db6990870907a881b97d34ae8f9ff4f
David Lee <david.lee@ichef.com.tw>
2018-02-12 16:13:21 +0800

    Add test

---
+++ queryfilter/tests/test_textfilters.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+from ..textfilters import TextFullyMatchedFilter
+
+
+class TestTextFullyMatchedFilter(object):
+    def test_something(self):
+        assert True
