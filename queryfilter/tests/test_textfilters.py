3928547669c7f8ebaebf8b5efe7ee87346ef3bb7
Keith Yang <yang@keitheis.org>
2018-02-12 17:25:14 +0800

    Minor refactor by TestTextFullyMatchedFilter.setup

--- queryfilter/tests/test_textfilters.py
+++ queryfilter/tests/test_textfilters.py
@@ -4,25 +4,22 @@ from ..textfilters import TextFullyMatchedFilter
 
 
 class TestTextFullyMatchedFilter(object):
+    def setup(self):
+        self.field_name_to_test = "name"
+        self.text_to_test = "name_example"
+        self.dicts = [
+            {self.field_name_to_test: self.text_to_test}
+        ]
+
     def test_text_match(self):
-        field_name_to_test = "name"
-        text_to_test = "name_example"
-        instance_to_test = {
-            field_name_to_test: text_to_test
-        }
-        text_filter = TextFullyMatchedFilter(field_name_to_test, {
-            "value": text_to_test
+        text_filter = TextFullyMatchedFilter(self.field_name_to_test, {
+            "value": self.text_to_test
         })
-        assert len(text_filter.on_dicts([instance_to_test])) == 1
+        assert len(text_filter.on_dicts(self.dicts)) == 1
 
     def test_text_does_not_match_should_fail(self):
-        field_name_to_test = "name"
-        text_to_test = "name_example"
-        wrong_text_to_test = "not_name_example"
-        instance_to_test = {
-            field_name_to_test: text_to_test
-        }
-        text_filter = TextFullyMatchedFilter(field_name_to_test, {
-            "value": wrong_text_to_test
+        text_not_match = "not_name_example"
+        text_filter = TextFullyMatchedFilter(self.field_name_to_test, {
+            "value": text_not_match
         })
-        assert len(text_filter.on_dicts([instance_to_test])) == 0
+        assert len(text_filter.on_dicts(self.dicts)) == 0
