2e9ff92506035e5f9ab673f1776e27b2e6deb303
David Lee <david.lee@ichef.com.tw>
2018-02-13 14:42:28 +0800

    Add test

--- queryfilter/tests/test_textfilters.py
+++ queryfilter/tests/test_textfilters.py
@@ -1,9 +1,9 @@
 from __future__ import absolute_import
 
-from ..textfilters import TextFullyMatchedFilter
+from ..textfilters import TextFullyMatchedFilter, TextPartialMatchedFilter
 
 
-class TestTextFullyMatchedFilter(object):
+class TestTextFilter(object):
     def setup(self):
         self.field_name_to_test = "name"
         self.text_to_test = "name_example"
@@ -11,15 +11,28 @@ class TestTextFullyMatchedFilter(object):
             {self.field_name_to_test: self.text_to_test}
         ]
 
-    def test_text_match(self):
+    def test_text_fully_match(self):
         text_filter = TextFullyMatchedFilter(self.field_name_to_test, {
             "value": self.text_to_test
         })
         assert len(text_filter.on_dicts(self.dicts)) == 1
 
-    def test_text_does_not_match_should_fail(self):
+    def test_text_does_not_fully_match_should_fail(self):
         text_not_match = "not_name_example"
         text_filter = TextFullyMatchedFilter(self.field_name_to_test, {
+            "value": text_not_match
+        })
+        assert len(text_filter.on_dicts(self.dicts)) == 0
+
+    def test_text_partial_match(self):
+        text_filter = TextPartialMatchedFilter(self.field_name_to_test, {
+            "value": self.text_to_test[:1]
+        })
+        assert len(text_filter.on_dicts(self.dicts)) == 1
+
+    def test_text_does_not_partial_match_should_fail(self):
+        text_not_match = "not"
+        text_filter = TextPartialMatchedFilter(self.field_name_to_test, {
             "value": text_not_match
         })
         assert len(text_filter.on_dicts(self.dicts)) == 0
