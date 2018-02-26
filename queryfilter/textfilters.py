4c1e7143fce15029497929c83e22732c672efd8f
David Lee <david.lee@ichef.com.tw>
2018-02-26 17:03:35 +0800

    Fix missing ()

--- queryfilter/textfilters.py
+++ queryfilter/textfilters.py
@@ -24,22 +24,22 @@ class TextMatchMixin(object):
 @QueryFilter.register_type_condition('string', 'equals')
 class TextFullyMatchedFilter(TextMatchMixin, FieldFilter):
     def _is_value_matched(self, value):
-        return bool(value == self.get_query_value)
+        return bool(value == self.get_query_value())
 
 
 @QueryFilter.register_type_condition('string', 'contains')
 class TextPartialMatchedFilter(TextMatchMixin, FieldFilter):
     def _is_value_matched(self, value):
-        return bool(self.get_query_value in value)
+        return bool(self.get_query_value() in value)
 
 
 @QueryFilter.register_type_condition('string', 'starts_with')
 class TextStartsWithMatchedFilter(TextMatchMixin, FieldFilter):
     def _is_value_matched(self, value):
-        return bool(value.startswith(self.get_query_value))
+        return bool(value.startswith(self.get_query_value()))
 
 
 @QueryFilter.register_type_condition('string', 'ends_with')
 class TextEndsWithMatchedFilter(TextMatchMixin, FieldFilter):
     def _is_value_matched(self, value):
-        return bool(value.endswith(self.get_query_value))
+        return bool(value.endswith(self.get_query_value()))
