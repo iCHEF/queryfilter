7863cc29211d2743cf201eeea19eb3237e6005c7
David Lee <david.lee@ichef.com.tw>
2018-03-05 09:51:21 +0800

    Oops, rename it back

--- queryfilter/textfilters.py
+++ queryfilter/textfilters.py
@@ -9,7 +9,7 @@ from .queryfilter import QueryFilter
 @add_metaclass(abc.ABCMeta)
 class TextMatchMixin(object):
     @abc.abstractmethod
-    def is_value_matched(self, value): pass
+    def _is_value_matched(self, value): pass
 
     def get_query_value(self):
         return self.filter_args["value"]
@@ -17,29 +17,29 @@ class TextMatchMixin(object):
     def on_dicts(self, dicts):
         return [
             d for d in dicts
-            if self.is_value_matched(d.get(self.field_name))
+            if self._is_value_matched(d.get(self.field_name))
         ]
 
 
 @QueryFilter.register_type_condition('string', 'equals')
 class TextFullyMatchedFilter(TextMatchMixin, FieldFilter):
-    def is_value_matched(self, value):
+    def _is_value_matched(self, value):
         return bool(value == self.get_query_value())
 
 
 @QueryFilter.register_type_condition('string', 'contains')
 class TextPartialMatchedFilter(TextMatchMixin, FieldFilter):
-    def is_value_matched(self, value):
+    def _is_value_matched(self, value):
         return bool(self.get_query_value() in value)
 
 
 @QueryFilter.register_type_condition('string', 'starts_with')
 class TextStartsWithMatchedFilter(TextMatchMixin, FieldFilter):
-    def is_value_matched(self, value):
+    def _is_value_matched(self, value):
         return bool(value.startswith(self.get_query_value()))
 
 
 @QueryFilter.register_type_condition('string', 'ends_with')
 class TextEndsWithMatchedFilter(TextMatchMixin, FieldFilter):
-    def is_value_matched(self, value):
+    def _is_value_matched(self, value):
         return bool(value.endswith(self.get_query_value()))
