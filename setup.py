a838c9463cfe9a53e707f7716f60c8bce9da69f2
David Lee <david.lee@ichef.com.tw>
2018-03-05 11:37:24 +0800

    Bump to 0.1.1 for pypl

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ required_dev = [
 
 setup(
     name="queryfilter",
-    version="0.1.0",
+    version="0.1.1",
     description=("Allow same query interface to be shared between Django ORM,"
                  "SQLAlchemy, and GraphQL backend."),
     long_description=open(os.path.join(source_root, "README.md")).read(),
