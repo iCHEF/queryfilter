53faf58bc7679624b258f4b16057a5b0bdaa4111
David Lee <david.lee@ichef.com.tw>
2018-03-26 15:09:47 +0800

    Fix test

--- queryfilter/examples/schema.py
+++ queryfilter/examples/schema.py
@@ -3,62 +3,35 @@ import graphene
 from queryfilter import QueryFilter
 
 from queryfilter.schema import (
-    TextFilterQueryObject, NumberRangeFilterQueryObject,
+    TextFilterQueryObject,
+    NumberRangeFilterQueryObject,
     SelectFloatFilterQueryObject
 )
 
-
-class KeyCanBeAccessedAsAttributes(dict):
-    """
-    Allow graphene to access dict value directly. 
-    """
-    def __init__(self, *args, **kwargs):
-        super(KeyCanBeAccessedAsAttributes, self).__init__(*args, **kwargs)
-        for arg in args:
-            if isinstance(arg, dict):
-                for k, v in arg.iteritems():
-                    self[k] = v
-
-        if kwargs:
-            for k, v in kwargs.iteritems():
-                self[k] = v
-
-    def __getattr__(self, attr):
-        return self.get(attr)
-
-    def __setattr__(self, key, value):
-        self.__setitem__(key, value)
-
-    def __setitem__(self, key, value):
-        super(KeyCanBeAccessedAsAttributes, self).__setitem__(key, value)
-        self.__dict__.update({key: value})
-
-    def __delattr__(self, item):
-        self.__delitem__(item)
-
-    def __delitem__(self, key):
-        super(KeyCanBeAccessedAsAttributes, self).__delitem__(key)
-        del self.__dict__[key]
-
-
-EXAMPLE_DATA = [
-            KeyCanBeAccessedAsAttributes({
-                "name": "A",
-                "price": 100,
-                "type": 0
-            }),
-            KeyCanBeAccessedAsAttributes({
-                "name": "B",
-                "price": 5,
-                "type": 1
-            }),
-        ]
+EXAMPLE_DATA = [{
+    "name": "A",
+    "price": 100,
+    "type": 0
+}, {
+    "name": "B",
+    "price": 5,
+    "type": 1
+}]
 
 
 class ExampleDataSchema(graphene.ObjectType):
     name = graphene.String()
     price = graphene.Float()
     type = graphene.Float()
+
+    def resolve_name(self, info):
+        return self.get("name")
+
+    def resolve_price(self, info):
+        return self.get("price")
+
+    def resolve_type(self, info):
+        return self.get("type")
 
 
 class ExampleQueryFilter(graphene.InputObjectType):
@@ -69,8 +42,7 @@ class ExampleQueryFilter(graphene.InputObjectType):
 
 class ExampleSchemaWithFilter(graphene.ObjectType):
     something = graphene.List(
-        ExampleDataSchema, queryFilter=ExampleQueryFilter()
-    )
+        ExampleDataSchema, queryFilter=ExampleQueryFilter())
 
     def resolve_something(self, info, **kwargs):
         query_filter = kwargs.get('queryFilter', {})
