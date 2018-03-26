ecb19e8a2a792d18043463d7780a77a9c67ec55d
David Lee <david.lee@ichef.com.tw>
2018-03-26 14:01:05 +0800

    GraphQL schema & example

---
+++ queryfilter/examples/schema.py
@@ -0,0 +1,35 @@
+import graphene
+
+from queryfilter import QueryFilter
+
+from queryfilter.schema import (
+    TextFilterQueryObject, NumberRangeFilterQueryObject,
+    SelectStringFilterQueryObject
+)
+
+
+class ExampleDataSchema(graphene.ObjectType):
+    name = graphene.String()
+    price = graphene.Float()
+    type = graphene.Float()
+
+
+class ExampleQueryFilter(graphene.ObjectType):
+    name = TextFilterQueryObject()
+    price = NumberRangeFilterQueryObject()
+    type = SelectStringFilterQueryObject()
+
+
+class ExampleSchemaWithFilter(graphene.ObjectType):
+    something = graphene.Field(
+        ExampleDataSchema, name="example", queryFilter=ExampleQueryFilter()
+    )
+
+    def resolve_something(self, info, **kwargs):
+        examples = []
+        query_filter = kwargs.get('reportFilter', {})
+
+        query_filter = QueryFilter(query_filter)
+        examples = query_filter.on_dicts(examples)
+
+        return examples
---
+++ queryfilter/schema.py
@@ -0,0 +1,65 @@
+import graphene
+
+TYPE_FIELD_DESC = "Don't modify if you're not using customized filter"
+
+
+class TextFilterType(graphene.Enum):
+    EQUALS = 'equals'
+    CONTAINS = 'contains'
+    STARTS_WITH = 'starts_with'
+    ENDS_WITH = 'ends_with'
+
+
+class TextFilterQueryObject(graphene.InputObjectType):
+    type = graphene.String(
+        default_value="string",
+        description=TYPE_FIELD_DESC
+    )
+    condition = graphene.Field(TextFilterType)
+    value = graphene.String(description="text to query", required=True)
+
+
+class SelectStringFilterQueryObject(graphene.InputObjectType):
+
+    type = graphene.String(
+        default_value="select",
+        description=TYPE_FIELD_DESC
+    )
+
+    values = graphene.List(
+        graphene.String, description="text list to query", required=True
+    )
+
+
+class SelectFloatFilterQueryObject(graphene.InputObjectType):
+
+    type = graphene.String(
+        default_value="select",
+        description=TYPE_FIELD_DESC
+    )
+
+    values = graphene.List(
+        graphene.Float, description="number list to query", required=True
+    )
+
+
+class NumberRangeFilterQueryObject(graphene.InputObjectType):
+    type = graphene.String(
+        default_value="number",
+        description=TYPE_FIELD_DESC
+    )
+    min = graphene.Float()
+    max = graphene.Float()
+
+    @property
+    def invalid_message(self):
+        return self.id.startswith('userid_')
+
+
+class BirthFilterQueryObject(graphene.InputObjectType):
+    type = graphene.String(
+        default_value="birthday",
+        description=TYPE_FIELD_DESC
+    )
+    start = graphene.String(default="01/01")
+    end = graphene.String(default="12/31")
