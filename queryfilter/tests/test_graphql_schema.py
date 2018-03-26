513ea4095a975220094ebc6961c7f261dde87dd3
David Lee <david.lee@ichef.com.tw>
2018-03-26 15:18:48 +0800

    Fix format

--- queryfilter/tests/test_graphql_schema.py
+++ queryfilter/tests/test_graphql_schema.py
@@ -10,14 +10,14 @@ class TestGraphQLSchema(object):
         client = Client(example_schema)
         target_data_name = EXAMPLE_DATA[0].get("name")
         query = '''
-            { 
+            {
                 something(queryFilter: {
                     name: {
                         value: "%s"
                     }
                 }){
                     name
-                } 
+                }
             }
         ''' % target_data_name
         executed = client.execute(query)
@@ -25,4 +25,3 @@ class TestGraphQLSchema(object):
         assert len(data["something"]) == 1
         query_result = data["something"][0]
         assert query_result.get("name") == target_data_name
-
