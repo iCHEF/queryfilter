de90abd860c7e4e3d7556b85dba25a97812c5475
David Lee <david.lee@ichef.com.tw>
2018-03-27 17:24:17 +0800

    Complete tests

--- queryfilter/examples/schema.py
+++ queryfilter/examples/schema.py
@@ -2,20 +2,26 @@ import graphene
 
 from queryfilter import QueryFilter
 
-from queryfilter.schema import (
-    TextFilterQueryObject,
-    NumberRangeFilterQueryObject,
-    SelectFloatFilterQueryObject
+from queryfilter.schemas import (
+    TextFilterQueryType,
+    NumberRangeFilterQueryType,
+    SelectFloatFilterQueryType,
+    SelectStringFilterQueryType,
+    BirthFilterQueryType
 )
 
 EXAMPLE_DATA = [{
     "name": "A",
     "price": 100,
-    "type": 0
+    "type": 0,
+    "category": "X",
+    "birthday": "05/20"
 }, {
     "name": "B",
     "price": 5,
-    "type": 1
+    "type": 1,
+    "category": "Y",
+    "birthday": "12/20"
 }]
 
 
@@ -23,6 +29,8 @@ class ExampleDataSchema(graphene.ObjectType):
     name = graphene.String()
     price = graphene.Float()
     type = graphene.Float()
+    category = graphene.String()
+    birthday = graphene.String()
 
     def resolve_name(self, info):
         return self.get("name")
@@ -33,11 +41,19 @@ class ExampleDataSchema(graphene.ObjectType):
     def resolve_type(self, info):
         return self.get("type")
 
+    def resolve_category(self, info):
+        return self.get("category")
+
+    def resolve_birthday(self, info):
+        return self.get("birthday")
+
 
 class ExampleQueryFilter(graphene.InputObjectType):
-    name = TextFilterQueryObject()
-    price = NumberRangeFilterQueryObject()
-    type = SelectFloatFilterQueryObject()
+    name = TextFilterQueryType()
+    price = NumberRangeFilterQueryType()
+    type = SelectFloatFilterQueryType()
+    category = SelectStringFilterQueryType()
+    birthday = BirthFilterQueryType()
 
 
 class ExampleSchemaWithFilter(graphene.ObjectType):
--- queryfilter/schemas.py
+++ queryfilter/schemas.py
@@ -3,25 +3,29 @@ import graphene
 TYPE_FIELD_DESC = "Don't modify if you're not using customized filter"
 
 
-class TextFilterType(graphene.Enum):
+class TextFilterTypes(graphene.Enum):
     EQUALS = 'equals'
     CONTAINS = 'contains'
     STARTS_WITH = 'starts_with'
     ENDS_WITH = 'ends_with'
 
 
-class TextFilterQueryObject(graphene.InputObjectType):
+class BirthdayFilterTypes(graphene.Enum):
+    DATE_RANGE = 'date_range'
+
+
+class TextFilterQueryType(graphene.InputObjectType):
     type = graphene.String(
         default_value="string",
         description=TYPE_FIELD_DESC
     )
     condition = graphene.Field(
-        TextFilterType, default_value=TextFilterType.EQUALS.value
+        TextFilterTypes, default_value=TextFilterTypes.EQUALS.value
     )
     value = graphene.String(description="text to query", required=True)
 
 
-class SelectStringFilterQueryObject(graphene.InputObjectType):
+class SelectStringFilterQueryType(graphene.InputObjectType):
 
     type = graphene.String(
         default_value="select",
@@ -33,7 +37,7 @@ class SelectStringFilterQueryObject(graphene.InputObjectType):
     )
 
 
-class SelectFloatFilterQueryObject(graphene.InputObjectType):
+class SelectFloatFilterQueryType(graphene.InputObjectType):
 
     type = graphene.String(
         default_value="select",
@@ -45,7 +49,7 @@ class SelectFloatFilterQueryObject(graphene.InputObjectType):
     )
 
 
-class NumberRangeFilterQueryObject(graphene.InputObjectType):
+class NumberRangeFilterQueryType(graphene.InputObjectType):
     type = graphene.String(
         default_value="number",
         description=TYPE_FIELD_DESC
@@ -58,10 +62,13 @@ class NumberRangeFilterQueryObject(graphene.InputObjectType):
         return self.id.startswith('userid_')
 
 
-class BirthFilterQueryObject(graphene.InputObjectType):
+class BirthFilterQueryType(graphene.InputObjectType):
     type = graphene.String(
         default_value="birthday",
         description=TYPE_FIELD_DESC
+    )
+    condition = graphene.Field(
+        BirthdayFilterTypes, default_value=BirthdayFilterTypes.DATE_RANGE.value
     )
     start = graphene.String(default="01/01")
     end = graphene.String(default="12/31")
